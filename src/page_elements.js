46aac1d473399b0bc24d753a27b330e982dd7449
jonahBerlyne <jonshaber@gmail.com>
2021-11-10 14:35:39 -0500

    image

--- src/page_elements.js
+++ src/page_elements.js
@@ -5,9 +5,18 @@ export default function showElements() {
  h1.textContent = "The Krusty Krab: Home of the Krabby Patties!";
  content.appendChild(h1);
 
- const img = document.createElement("img");
- img.src = "/krustykrab.jpeg";
- content.appendChild(img);
+ // const img = document.createElement("img");
+ // img.src = "/krustykrab.jpeg";
+ // content.appendChild(img);
+
+ var img = new Image();
+ img.className = 'center';
+ img.width = 300;
+ img.height = 300;
+ img.src = '/krustykrab.jpeg';
+ img.onload = function() {
+    content.appendChild(img);
+ };
 
  const p = document.createElement("p");
  p.textContent = "This is a fantastic burger joint. If you want to eat krabby patties, then the krusty krab is where it's at boss.";
